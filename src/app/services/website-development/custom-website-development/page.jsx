ee5dfa132d1309c1a4c0d13f01d855468eeec27e
waseemlinuxfreakz <89018806+waseemlinuxfreakz@users.noreply.github.com>
2026-04-10 15:15:49 +0500

    website inner pages done

--- src/app/services/website-development/custom-website-development/page.jsx
+++ src/app/services/website-development/custom-website-development/page.jsx
@@ -4,6 +4,9 @@ export const metadata = {
   title: "High-Performance Custom Websites | DOTOLI DIGITAL",
   description:
     "Scale your business with custom websites from DOTOLI DIGITAL. Performance-focused development that improves SEO visibility, UX, and conversion rates.",
+  alternates: {
+    canonical: "",
+  },
 };
 export default function Page() {
   return <CustomWebsiteDevelopment />;
--- src/app/services/website-development/ecommerce-website-development/page.jsx
+++ src/app/services/website-development/ecommerce-website-development/page.jsx
@@ -4,6 +4,9 @@ export const metadata = {
   title: "Scalable eCommerce Development | DOTOLI DIGITAL",
   description:
     "Grow your online store with DOTOLI DIGITAL’s scalable eCommerce development. Reliable, secure, and optimized websites that convert visitors into customers.",
+  alternates: {
+    canonical: "",
+  },
 };
 export default function Page() {
   return <EcommerceWebsiteDevelopment />;
