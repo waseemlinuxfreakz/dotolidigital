f1a6691738f65887dfe09f0093f0ee7ee824658b
waseemlinuxfreakz <89018806+waseemlinuxfreakz@users.noreply.github.com>
2026-04-10 15:15:12 +0500

    website tag

--- src/app/services/website-development/landing-page-design-development/page.jsx
+++ src/app/services/website-development/landing-page-design-development/page.jsx
@@ -4,6 +4,9 @@ export const metadata = {
   title: "Lead-Focused Landing Pages | DOTOLI DIGITAL",
   description:
     "Get more leads with DOTOLI DIGITAL’s landing page design & development. Pages built to communicate value and increase conversions.",
+  alternates: {
+    canonical: "",
+  },
 };
 export default function Page() {
   return <LandingPageDesignDevelopment />;
