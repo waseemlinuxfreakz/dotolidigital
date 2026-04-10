fe7808f60a6fa480280a38939fa9865833bcac78
bilalsafdar111-cpu <bilalsafdar111@gmail.com>
2026-04-10 16:25:54 +0500

    updated

--- src/app/services/website-development/ecommerce-website-development/page.jsx
+++ src/app/services/website-development/ecommerce-website-development/page.jsx
@@ -4,11 +4,8 @@ export const metadata = {
   title: "Scalable eCommerce Development | DOTOLI DIGITAL",
   description:
     "Grow your online store with DOTOLI DIGITAL’s scalable eCommerce development. Reliable, secure, and optimized websites that convert visitors into customers.",
-    alternates: {
-    canonical: "https://www.dotolidigital.com/services/website-development/ecommerce-website-development",
-  },
   alternates: {
-    canonical: "",
+    canonical: "https://www.dotolidigital.com/services/website-development/ecommerce-website-development",
   },
 };
 export default function Page() {
