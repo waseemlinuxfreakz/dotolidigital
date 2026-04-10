5a3d443bcc6b8207d3ebb285f795a1327cecfcf8
bilalsafdar111-cpu <bilalsafdar111@gmail.com>
2026-04-10 16:24:21 +0500

    updated

--- src/app/services/website-development/custom-website-development/page.jsx
+++ src/app/services/website-development/custom-website-development/page.jsx
@@ -4,8 +4,7 @@ export const metadata = {
   title: "High-Performance Custom Websites | DOTOLI DIGITAL",
   description:
     "Scale your business with custom websites from DOTOLI DIGITAL. Performance-focused development that improves SEO visibility, UX, and conversion rates.",
-    
-  alternates: {
+    alternates: {
     canonical: "https://www.dotolidigital.com/services/website-development/custom-website-development",
   },
 };
