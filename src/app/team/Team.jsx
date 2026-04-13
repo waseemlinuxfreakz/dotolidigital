3b2b5ddbcd514d477f7e069d30f8151b604eee49
waseemlinuxfreakz <89018806+waseemlinuxfreakz@users.noreply.github.com>
2026-04-13 17:38:27 +0500

    other menu pages updated

---
+++ src/app/team/Team.jsx
@@ -0,0 +1,13 @@
+import Team from "./Team";
+
+export const metadata = {
+  title: "Lead-Focused Landing Pages | DOTOLI DIGITAL",
+  description:
+    "Get more leads with DOTOLI DIGITAL’s landing page design & development. Pages built to communicate value and increase conversions.",
+  alternates: {
+    canonical: "https://www.dotolidigital.com/services/website-development/landing-page-design-development",
+  },
+};
+export default function Page() {
+  return <Team />;
+}
